81c9ee6e4fd3eee4909331f16bddcd8657fe0564
skisenge <skisengese@gmail.com>
2025-05-25 00:31:34 +0300

    feat: add user registration

--- src/auth.c
+++ src/auth.c
@@ -1,8 +1,6 @@
 #include <termios.h>
 #include "header.h"
 
-char *USERS = "./data/users.txt";
-
 void loginMenu(char a[50], char pass[50])
 {
     struct termios oflags, nflags;
@@ -20,7 +18,7 @@ void loginMenu(char a[50], char pass[50])
     if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
     {
         perror("tcsetattr");
-        return exit(1);
+        exit(1);
     }
     printf("\n\n\n\n\n\t\t\t\tEnter the password to login:");
     scanf("%s", pass);
@@ -29,31 +27,108 @@ void loginMenu(char a[50], char pass[50])
     if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
     {
         perror("tcsetattr");
-        return exit(1);
-    }
-};
-
-const char *getPassword(struct User u)
-{
-    FILE *fp;
-    struct User userChecker;
-
-    if ((fp = fopen("./data/users.txt", "r")) == NULL)
-    {
-        printf("Error! opening file");
         exit(1);
     }
+}
 
-    while (fscanf(fp, "%s %s", userChecker.name, userChecker.password) != EOF)
+void registerMenu(char a[50], char pass[50])
+{
+    struct termios oflags, nflags;
+
+    system("clear");
+    printf("\n\n\n\t\t\t\t   Bank Management System\n\t\t\t\t\t User Registration:");
+    printf("\n\t\t\t\tEnter username:");
+    scanf("%s", a);
+
+    // disabling echo
+    tcgetattr(fileno(stdin), &oflags);
+    nflags = oflags;
+    nflags.c_lflag &= ~ECHO;
+    nflags.c_lflag |= ECHONL;
+
+    if (tcsetattr(fileno(stdin), TCSANOW, &nflags) != 0)
     {
-        if (strcmp(userChecker.name, u.name) == 0)
-        {
-            fclose(fp);
-            char *buff = userChecker.password;
-            return buff;
-        }
+        perror("tcsetattr");
+        exit(1);
+    }
+    printf("\n\n\n\n\n\t\t\t\tEnter password:");
+    scanf("%s", pass);
+
+    // restore terminal
+    if (tcsetattr(fileno(stdin), TCSANOW, &oflags) != 0)
+    {
+        perror("tcsetattr");
+        exit(1);
+    }
+}
+
+int getPassword(struct User *u)
+{
+    sqlite3 *db = getDatabase();
+    sqlite3_stmt *stmt;
+
+    char sql[] = "SELECT id, password FROM users WHERE name = ?";
+
+    int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+        return 0;
     }
 
-    fclose(fp);
-    return "no user found";
+    sqlite3_bind_text(stmt, 1, u->name, -1, SQLITE_STATIC);
+
+    rc = sqlite3_step(stmt);
+    if (rc == SQLITE_ROW)
+    {
+        u->id = sqlite3_column_int(stmt, 0);
+        strcpy(u->password, (char *)sqlite3_column_text(stmt, 1));
+        sqlite3_finalize(stmt);
+        return 1;
+    }
+
+    sqlite3_finalize(stmt);
+    return 0;
+}
+
+int registerUser(char name[50], char password[50])
+{
+    sqlite3 *db = getDatabase();
+    sqlite3_stmt *stmt;
+
+    // Check if user already exists
+    char check_sql[] = "SELECT COUNT(*) FROM users WHERE name = ?";
+    int rc = sqlite3_prepare_v2(db, check_sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+        return 0;
+    }
+
+    sqlite3_bind_text(stmt, 1, name, -1, SQLITE_STATIC);
+    rc = sqlite3_step(stmt);
+
+    if (rc == SQLITE_ROW && sqlite3_column_int(stmt, 0) > 0)
+    {
+        sqlite3_finalize(stmt);
+        return 0; // User already exists
+    }
+    sqlite3_finalize(stmt);
+
+    // Insert new user
+    char insert_sql[] = "INSERT INTO users (name, password) VALUES (?, ?)";
+    rc = sqlite3_prepare_v2(db, insert_sql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK)
+    {
+        fprintf(stderr, "Failed to prepare statement: %s\n", sqlite3_errmsg(db));
+        return 0;
+    }
+
+    sqlite3_bind_text(stmt, 1, name, -1, SQLITE_STATIC);
+    sqlite3_bind_text(stmt, 2, password, -1, SQLITE_STATIC);
+
+    rc = sqlite3_step(stmt);
+    sqlite3_finalize(stmt);
+
+    return (rc == SQLITE_DONE) ? 1 : 0;
 }
