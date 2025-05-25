9421d60ff44c70a33d3c118699d5ed8c3881a4b4
skisenge <skisengese@gmail.com>
2025-05-26 00:58:54 +0300

    feat: update header file

--- src/header.h
+++ src/header.h
@@ -20,7 +20,7 @@ struct Record
     int userId;
     char name[100];
     char country[100];
-    int phone;
+    char phone[20];
     char accountType[10];
     int accountNbr;
     double amount;
@@ -39,12 +39,20 @@ struct User
 int initDatabase();
 sqlite3 *getDatabase();
 void closeDatabase();
+int accountExists(int accountId);
+int validateDate(int month, int day, int year);
+int validateAccountType(char *accountType);
+int validateName(char *name);
+int validatePhone(const char *phoneStr);
+int validateAmount(const char *amountStr);
+int validateAccountNumber(const char *acctNum);
 
 // authentication functions
 void loginMenu(char a[50], char pass[50]);
 void registerMenu(char a[50], char pass[50]);
 int getPassword(struct User *u);
 int registerUser(char name[50], char password[50]);
+int validatePassword(const char *pass);
 
 // system function
 void createNewAcc(struct User u);
